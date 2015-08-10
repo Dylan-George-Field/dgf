16d629046acb2b0a8b65b8de84fc6b93cce2200b
Dylan-George-Field <dylan.george.field@gmail.com>
2015-08-10 23:26:58 +1000

    Update youtubeDateChanger.php

--- youtubeDateChanger.php
+++ youtubeDateChanger.php
@@ -2,7 +2,6 @@
 /* This file will change dates into YouTube style "one day ago, month ago, year ago" style */
 /* I'm using the input format get_the_time('d/m/Y'), ensure you use get_the_time() and not get_the_date() */
 /* I'll have to change this simply to '1-7 days ago', '1-4 weeks ago', '1-12 months ago', '1-9999 years ago' */ 
-/* Dylan George Field - dylan@georgefield.com.au */
 
 function youtubeDateChanger ($date) {
 	$result = "Unknown";
