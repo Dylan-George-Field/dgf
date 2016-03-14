19172e56fbdfd4bacd0a682e171e004a7f39d8ca
Dylan Field <DField@agl.com.au>
2016-03-14 17:43:03 +1100

    This is a commit to something

---
+++ aFile.php
@@ -0,0 +1,6 @@
+<?php
+/* This file will change dates into YouTube style "one day ago, month ago, year ago" style */
+/* I'm using the input format get_the_time('d/m/Y'), ensure you use get_the_time() and not get_the_date() */
+/* I'll have to change this simply to '1-7 days ago', '1-4 weeks ago', '1-12 months ago', '1-9999 years ago' */ 
+/* Modulus would be useful here */
+?>
